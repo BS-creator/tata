252c9eb7195ea6eddb2268b9b287a53fcde7929d
Julien Bisconti <julien.bisconti@gmail.com>
2014-08-20 17:03:31 +0200

    * Print JSON from backend: working

--- public/js/itransfer.js
+++ public/js/itransfer.js
@@ -1,51 +1,26 @@
+'user strict'
+
 $( document ).ready(function() {
-
-
     // sidenav //////////////////////////////////
-
     $("#sidenav h3").click(function(){
         $(this).next("ul").toggle();
     });
+});
+
+$(function() {
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8009/file/hbrchdbridmxycvroqthk445/',
+        success:function(resJSON){
+            var templateSource   = $("#datatmpl").html(),
+                template = Handlebars.compile(templateSource),
+                tableHTML = template(resJSON);
+            $('#mycontainerfortemplate').html(tableHTML);
+
+        }})
 
 
 
-        var label = [];
-
-
-
-    var d = $.getJSON("scripts/files.json", function (data) {
-
-
-        //var arr = $.map( data, function( obj, i ) { return data; } );
-        $.each( data, function( index, obj ) {
-            $.each(obj, function(key,val){
-                if(key=="LIB_CATEGORIE_F"){
-                    //console.log(label[val]);
-                    if(label.indexOf(val)===-1){
-                        label.push(val);
-
-                    }
-                }
-            });
-            //items.push( "<li id='" + key + "'>" + val + "</li>" );
-
-        });
-    })
-    //console.log(data);
-    .done(function () {
-            console.log("import JSON success");
-            console.log(label);
-        })
-    .fail(function () {
-            console.log("import JSON error");
-        });
-
-
-
-
-    // INCLUDING FILES
-    $(function(){
-        $("#content").load("view/index.file-list.html");
-    });
 
 });
