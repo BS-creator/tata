e70f27a9e4de60292be9e1f9864296e46af536a2
Gilles Collard <gilles.collard@groups.be>
2014-08-21 09:25:31 +0200

    less modular

--- public/js/itransfer.js
+++ public/js/itransfer.js
@@ -1,21 +1,9 @@
 $( document ).ready(function() {
 
 
-    // sidenav //////////////////////////////////
-
-    $("#sidenav h3").click(function(){
-        $(this).next("ul").toggle();
-    });
-
-
-
-        var label = [];
-
-
+    var label = [];
 
     var d = $.getJSON("scripts/files.json", function (data) {
-
-
         //var arr = $.map( data, function( obj, i ) { return data; } );
         $.each( data, function( index, obj ) {
             $.each(obj, function(key,val){
@@ -23,12 +11,9 @@ $( document ).ready(function() {
                     //console.log(label[val]);
                     if(label.indexOf(val)===-1){
                         label.push(val);
-
                     }
                 }
             });
-            //items.push( "<li id='" + key + "'>" + val + "</li>" );
-
         });
     })
     //console.log(data);
@@ -37,15 +22,14 @@ $( document ).ready(function() {
             console.log(label);
         })
     .fail(function () {
-            console.log("import JSON error");
-        });
-
-
-
+        console.log("import JSON error");
+    });
 
     // INCLUDING FILES
+    /*
     $(function(){
         $("#content").load("view/index.file-list.html");
     });
+    */
 
 });
---
+++ public/js/prestaweb-comp.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+    // SIDENAV
+    $("#sidenav .sidenav-head").click(function(){
+        $(this).next("ul").toggle();
+    });
+
+
+
+
+
+});
