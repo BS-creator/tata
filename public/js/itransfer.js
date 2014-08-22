4ba646421462c6f1aab4416df14db3c0af36548c
Julien Bisconti <julien.bisconti@gmail.com>
2014-08-22 16:17:15 +0200

    * testing bootstrap-table

--- public/js/itransfer.js
+++ public/js/itransfer.js
@@ -1,6 +1,6 @@
 'user strict'
 
-$(function() {
+$(function () {
     //var $mainTable = $('#mainTable > tbody:last');
 
     var lineObj = {};
@@ -16,11 +16,12 @@ $(function() {
             "    <td>{{ext}}</td>" +
             "</tr>");
 
-    function addFile(file){
+    function addFile(file) {
         $('#mainBody').append(template(file));
     }
 
-    function convertJSONObj(obj){   }
+    function convertJSONObj(obj) {
+    }
 
     $.ajax({
         type: 'GET',
@@ -32,18 +33,18 @@ $(function() {
                 var filename = item.filename;
                 var dl = (item.downloadCount == 0);
                 lineObj = {
-                    "file"              : filename.substr(0,5),
-                    "typeDoc"           : filename.substr(6,4),
-                    "noEmp"             : filename.substr(11,6),
-                    "refCpl"            : filename.substr(18,6),
-                    "bla2"              : filename.substr(25,6),
-                    "date"              : filename.substr(32,8),
-                    "fileDesc"          : filename.substr(41,6),
-                    "ext"               : filename.substr(48,3),
-                    "id"                : item.idFile,
-                    "fileName"          : filename,
-                    "ftpNew"            : item.isNew,
-                    "notDownloaded"     : dl
+                    "file": filename.substr(0, 5),
+                    "typeDoc": filename.substr(6, 4),
+                    "noEmp": filename.substr(11, 6),
+                    "refCpl": filename.substr(18, 6),
+                    "bla2": filename.substr(25, 6),
+                    "date": filename.substr(32, 8),
+                    "fileDesc": filename.substr(41, 6),
+                    "ext": filename.substr(48, 3),
+                    "id": item.idFile,
+                    "fileName": filename,
+                    "ftpNew": item.isNew,
+                    "notDownloaded": dl
                 }
                 addFile(lineObj);
             });
@@ -56,9 +57,60 @@ $(function() {
         }
     });
 
-    $('#mainTable').delegate('.dl', 'click', function(){
+    $('#mainTable').delegate('.dl', 'click', function () {
         $(this).attr('href', 'http://172.20.20.64:8009/file/jxaqei45ibgph3bmuup4e345/' + $(this).attr('data-id') + '/' + $(this).attr('data-file'));
 
     });
-});
 
+    $('#table-javascript').bootstrapTable({
+        method: 'get',
+        url: 'data2.json',
+        height: 400,
+        striped: true,
+        pagination: true,
+        pageSize: 50,
+        pageList: [10, 25, 50, 100, 200],
+        search: true,
+        showColumns: true,
+        showRefresh: true,
+        minimunCountColumns: 2,
+        columns: [
+            {
+                field: 'state',
+                checkbox: true
+            },
+            {
+                field: 'id',
+                title: 'Item ID',
+                align: 'right',
+                valign: 'bottom',
+                sortable: true
+            },
+            {
+                field: 'name',
+                title: 'Item Name',
+                align: 'center',
+                valign: 'middle',
+                sortable: true,
+                formatter: nameFormatter
+            },
+            {
+                field: 'price',
+                title: 'Item Price',
+                align: 'left',
+                valign: 'top',
+                sortable: true,
+                formatter: priceFormatter,
+                sorter: priceSorter
+            },
+            {
+                field: 'operate',
+                title: 'Item Operate',
+                align: 'center',
+                valign: 'middle',
+                formatter: operateFormatter,
+                events: operateEvents
+            }
+        ]
+    });
+});
