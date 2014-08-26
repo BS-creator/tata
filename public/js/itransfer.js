6b69ae373c2e95574e1671a3316be17c9fb75e73
Julien Bisconti <julien.bisconti@gmail.com>
2014-08-26 09:40:00 +0200

    * testing bootstrap-table

--- public/js/itransfer.js
+++ public/js/itransfer.js
@@ -1,116 +1,88 @@
 'user strict'
 
-$(function () {
+$(function() {
     //var $mainTable = $('#mainTable > tbody:last');
 
     var lineObj = {};
 
+    var table =  $('#mainBody');
+
     var template = Handlebars.compile( //<input type='checkbox'/>
-            "<tr>" +
-            "    <td class='{{notDownloaded}}'><a class='dl' data-id='{{id}}' data-file='{{fileName}}' href='#'><i class='fa fa-download'></i> </a></td>" +
-            "    <td>{{date}}</td>" +
+            // "<tr class='{{ftpNew}}'>" +
+            "<tr class=''>" +
+            "    <td class='{{notDownloaded}}'><a class='dl' data-id='{{id}}' data-file='{{fileName}}' href='#'><i class='fa fa-download'></i>" +
+                " <small class='text-muted'>{{countDl}}</small> </a></td>" +
             "    <td>{{idFile}}</td>" +
+            "    <td>{{date}}</td>" +
             "    <td>{{file}}</td>" +
-            "    <td>{{typeDoc}}</td>" +
             "    <td>{{noEmp}}</td>" +
             "    <td>{{ext}}</td>" +
             "</tr>");
 
     function addFile(file) {
-        $('#mainBody').append(template(file));
+        table.append(template(file));
     }
 
-    function convertJSONObj(obj) {
+    function convertJSONObj(item){
+        //012345678901234567890123456789012345678901234567890
+        //FOPAI_0031_079983_000003_000181_20131022_000000.PDF
+        var filename = item.filename,
+            lineObj = {
+                "file": filename.substr(0, 5),
+                "typeDoc": filename.substr(6, 4),
+                "noEmp": filename.substr(11, 6),
+                "refCpl": filename.substr(18, 6),
+                "bla2": filename.substr(25, 6),
+                "date": filename.substr(32, 8),
+                "fileDesc": filename.substr(41, 6),
+                "ext": filename.substr(48, 3),
+                "id": item.idFile,
+                "fileName": filename,
+                "ftpNew": item.isNew,
+                "notDownloaded": (item.downloadCount == 0)
+            }
+    }
+
+    function tableLine(data) {
+        $('#mainTable').bootstrapTable({
+            /*      method: 'get',
+             url: 'http://deviapps.groups.be/ariane/file/2lclt345dkevwz45v0v32h45/',*/
+            height: 400,
+            striped: true,
+            pagination: true,
+            pageSize: 50,
+            pageList: [10, 25, 50, 100, 200],
+            search: true,
+            showColumns: true,
+            showRefresh: true,
+            minimumCountColumns: 2,
+            idField: "idFile"
+        });
+        /* $('#get-selections').click(function() {
+         alert('Selected values: ' + JSON.stringify($table.bootstrapTable('getSelections')));
+         });*/
+        $.each(data.data, function (i, item) {
+            addFile(convertJSONObj(item));
+        });
+        ///$('#mainTable').tablesorter();
+
+        /* $("#mainTable")
+         .tablesorter({widthFixed: true, widgets: ['zebra']})
+         .tablesorterPager({container: $("#pager")});  */
+
+
     }
 
     $.ajax({
         type: 'GET',
-        url: 'http://deviapps.groups.be/ariane/file/2lclt345dkevwz45v0v32h45/',
-        success: function (data) {
-            $.each(data.data, function (i, item) {
-                //012345678901234567890123456789012345678901234567890
-                //FOPAI_0031_079983_000003_000181_20131022_000000.PDF
-                var filename = item.filename;
-                var dl = (item.downloadCount == 0);
-                lineObj = {
-                    "file": filename.substr(0, 5),
-                    "typeDoc": filename.substr(6, 4),
-                    "noEmp": filename.substr(11, 6),
-                    "refCpl": filename.substr(18, 6),
-                    "bla2": filename.substr(25, 6),
-                    "date": filename.substr(32, 8),
-                    "fileDesc": filename.substr(41, 6),
-                    "ext": filename.substr(48, 3),
-                    "id": item.idFile,
-                    "fileName": filename,
-                    "ftpNew": item.isNew,
-                    "notDownloaded": dl
-                }
-                addFile(lineObj);
-            });
-            ///$('#mainTable').tablesorter();
-
-            $("#mainTable")
-                .tablesorter({widthFixed: true, widgets: ['zebra']})
-                .tablesorterPager({container: $("#pager")});
-
-        }
+        //url: 'http://deviapps.groups.be/ariane/file/2lclt345dkevwz45v0v32h45/',
+        url: 'http://localhost:8018/file/tyy2no55byypmuujm4fdvhzk',
+        success: tableLine
     });
 
-    $('#mainTable').delegate('.dl', 'click', function () {
-        $(this).attr('href', 'http://172.20.20.64:8009/file/jxaqei45ibgph3bmuup4e345/' + $(this).attr('data-id') + '/' + $(this).attr('data-file'));
+    /*$('#mainTable').delegate('.dl', 'click', function(){
+        $(this).attr('href', 'http://deviapps/ariane/file/2lclt345dkevwz45v0v32h45/' + $(this).attr('data-id') + '/' + $(this).attr('data-file'));
 
-    });
+    });*/
 
-    $('#table-javascript').bootstrapTable({
-        method: 'get',
-        url: 'data2.json',
-        height: 400,
-        striped: true,
-        pagination: true,
-        pageSize: 50,
-        pageList: [10, 25, 50, 100, 200],
-        search: true,
-        showColumns: true,
-        showRefresh: true,
-        minimunCountColumns: 2,
-        columns: [
-            {
-                field: 'state',
-                checkbox: true
-            },
-            {
-                field: 'id',
-                title: 'Item ID',
-                align: 'right',
-                valign: 'bottom',
-                sortable: true
-            },
-            {
-                field: 'name',
-                title: 'Item Name',
-                align: 'center',
-                valign: 'middle',
-                sortable: true,
-                formatter: nameFormatter
-            },
-            {
-                field: 'price',
-                title: 'Item Price',
-                align: 'left',
-                valign: 'top',
-                sortable: true,
-                formatter: priceFormatter,
-                sorter: priceSorter
-            },
-            {
-                field: 'operate',
-                title: 'Item Operate',
-                align: 'center',
-                valign: 'middle',
-                formatter: operateFormatter,
-                events: operateEvents
-            }
-        ]
-    });
 });
