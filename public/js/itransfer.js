93fccd08e72c59b090b113c12677fe6bb8f588b8
Julien Bisconti <julien.bisconti@gmail.com>
2014-08-26 13:49:18 +0200

    * adding data for tree view

--- public/js/itransfer.js
+++ public/js/itransfer.js
@@ -1,6 +1,279 @@
 'user strict'
 
-$(function() {
+$(function () {
+    // MENU
+
+   /* $('#jstree_demo').jstree({
+        "core" : {
+            "animation" : 0,
+            "check_callback" : true,
+            "themes" : { "stripes" : true },
+            'data' : {
+                'url' : function (node) {
+                    return node.id === '#' ?
+                        'ajax_demo_roots.json' : 'ajax_demo_children.json';
+                },
+                'data' : function (node) {
+                    return { 'id' : node.id };
+                }
+            }
+        },
+        "types" : {
+            "#" : {
+                "max_children" : 1,
+                "max_depth" : 4,
+                "valid_children" : ["root"]
+            },
+            "root" : {
+                "icon" : "/static/3.0.3/assets/images/tree_icon.png",
+                "valid_children" : ["default"]
+            },
+            "default" : {
+                "valid_children" : ["default","file"]
+            },
+            "file" : {
+                "icon" : "glyphicon glyphicon-file",
+                "valid_children" : []
+            }
+        },
+        "plugins" : [
+            "contextmenu", "dnd", "search",
+            "state", "types", "wholerow"
+        ]
+    });*/
+
+    $('#sidenav').jstree({
+        'core': {
+            'data': {
+                "text"  : "Tous les documents",
+                "state" : {
+                    "opened"    : true,
+                    "disabled"  : false,
+                    "selected"   : true
+                },
+                "children"    : [
+                    {
+                        "text"  : "0 - Documents Divers",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "1 - Documents de paie",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "2 - Décomptes",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "3 - Attestations",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "4 - Fichiers de données",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "5 - Paiements",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "6 - Rappels",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "7 - Titres repas",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "8 - Documents annuels",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "9 - Statistiques",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "10 - Comptabilisation",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "11 - Documents de chômage",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "12 - Documents DMFA",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "13 - Fichiers interfaces AFAS",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "14 - Documents externes (scan)",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    },{
+                        "text"  : "15 - Documents électroniques",
+                        "state" : {
+                            "opened"    : false,
+                            "disabled"  : false
+                        },
+                        "children"    : [
+                            {
+
+                            }
+                        ],
+                        "li_attr"     : {"class" : "sidenav-head"},
+                        "a_attr"      : {}
+                    }
+                ],
+                "li_attr"     : {"class" : "sidenav-head"},
+                "a_attr"      : {}
+
+            }
+        }
+    });
+
+
+
+
+
+    // TABLE
     $('#mainTable').bootstrapTable({
         method: 'get',
         url: 'http://localhost:8018/file/yinnye55e2iqbf55iah3th45/',
@@ -13,94 +286,110 @@ $(function() {
         showColumns: true,
         showRefresh: true,
         minimunCountColumns: 2,
-        columns: [{
-            field: 'state',
-            checkbox: true
-        }, {
-            field: 'notDownloaded',
-            title: 'DL',
-            align: 'right',
-            valign: 'bottom',
-            sortable: true
-        }, {
-            field: 'isNew',
-            title: 'New',
-            align: 'right',
-            valign: 'bottom',
-            sortable: true
-        }, {
-            field: 'idFile',
-            title: 'File ID',
-            align: 'right',
-            valign: 'bottom',
-            sortable: true
-        }, {
-            field: 'filename',
-            title: 'Name',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'path',
-            title: 'Path',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'size',
-            title: 'Size',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'libelle',
-            title: 'Libelle',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'typeDoc',
-            title: 'Document',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'noEmployeur',
-            title: 'Employeur',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'refClientCompl',
-            title: 'Ref Client',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'counter',
-            title: '#',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'date',
-            title: 'Date',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'refGroups',
-            title: 'Ref Group S',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }, {
-            field: 'extension',
-            title: 'ext',
-            align: 'center',
-            valign: 'middle',
-            sortable: true
-        }]
+        columns: [
+            {
+                field: 'state',
+                checkbox: true
+            },
+            {
+                field: 'notDownloaded',
+                title: 'DL',
+                align: 'right',
+                valign: 'bottom',
+                sortable: true
+            },
+            {
+                field: 'isNew',
+                title: 'New',
+                align: 'right',
+                valign: 'bottom',
+                sortable: true
+            },
+            {
+                field: 'idFile',
+                title: 'File ID',
+                align: 'right',
+                valign: 'bottom',
+                sortable: true
+            },
+            {
+                field: 'filename',
+                title: 'Name',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'path',
+                title: 'Path',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'size',
+                title: 'Size',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'libelle',
+                title: 'Libelle',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'typeDoc',
+                title: 'Document',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'noEmployeur',
+                title: 'Employeur',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'refClientCompl',
+                title: 'Ref Client',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'counter',
+                title: '#',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'date',
+                title: 'Date',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'refGroups',
+                title: 'Ref Group S',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            },
+            {
+                field: 'extension',
+                title: 'ext',
+                align: 'center',
+                valign: 'middle',
+                sortable: true
+            }
+        ]
     });
 
 });
