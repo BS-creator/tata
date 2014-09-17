043856762cca981e6b2d43d1d3c0a81aa3bae7fa
Julien Bisconti <julien.bisconti@gmail.com>
2014-09-17 15:13:16 +0200

    * improve breadcrumb

--- public/js/bootstrap-table.js
+++ public/js/bootstrap-table.js
@@ -709,7 +709,7 @@
 
         html.push('<div class="row"><div class="col-md-12">',
             '<button type="button" class="downloadall btn btn-success">' +
-                sprintf('<i class="fa fa-download"></i>&nbsp;&nbsp;%s</button>',this.options.formatDL()),
+                sprintf('<i class="fa fa-download"></i>&nbsp;&nbsp;%s</button>',this.options.formatDownloadAll()),
             '</div></div>');
         html.push('</div>',
             '<div class="text-center wide pagination">',
