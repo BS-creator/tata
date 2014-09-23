ff74f7bbe11baa4863965484e80c0bc376a7ed99
Julien Bisconti <julien.bisconti@gmail.com>
2014-09-23 12:40:34 +0200

    * i18n: improve translation

--- public/js/login.js
+++ public/js/login.js
@@ -17,7 +17,6 @@ $(function () {
         if (window.location.hostname.indexOf('localhost') > -1) {
             sessionStorage.setItem('baseURL', '//localhost:4000/');
             sessionStorage.setItem('serverURL', '//172.20.20.64:8018/'); // deviapps??
-
         } else if (window.location.hostname.indexOf('qaiapps') > -1) { //QA
             sessionStorage.setItem('baseURL', '//qaiapps.groups.be/itransfer/');
             sessionStorage.setItem('serverURL', '//qaiapps.groups.be/ariane/');
