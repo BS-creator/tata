53279bc8fd9782489e024e0f449a950233472540
Gilles Collard <gilles.collard@groups.be>
2014-10-02 12:58:13 +0200

    responsive side-menu slide in/out

---
+++ public/js/gst.js
@@ -0,0 +1,11 @@
+
+
+
+$('document').ready(function(){
+
+    // tooltip - add rel='tooltip' to the tooltip element
+    $(function () {
+        $("[rel='tooltip']").tooltip();
+    });
+
+});
--- public/js/side-menu.js
+++ public/js/side-menu.js
@@ -1,39 +1,51 @@
 $('document').ready(function(){
 
-    // resize side-menu height
+    ////////////////////////////////////////////////////////
+    //                  SIDE-MENU
+    ///////////////////////////////////////////////////////
+
+    // initialize size & display /default
+    (function() {
+        $('#side-menu').css({
+            top: $('#main').offset().top, // get top height to align
+            right: -$('.sidebar-left').width(),
+            width: $('.sidebar-left').width(),
+            height: $(window).height()- $('#main').offset().top
+        }).removeClass();
+    })();
+
+    // set position
+    var sideMenuToggle = function(displayed){
+        var MSAbsc = (displayed === true) ? 0 : -$('.sidebar-left').width();
+        var mainAbsc = (displayed === false) ? 0 : -$('.sidebar-left').width();
+        $('#main').animate({
+            right: -mainAbsc
+        });
+        $('#side-menu').animate({
+            right: MSAbsc,
+            width: $('.sidebar-left').width()
+        }).toggleClass('active');
+    }
+
+    // resize side-menu width x height
     $(window).resize(function() {
-        $('#side-menu').height($(window).height()- 89); //.height() - 85 depends on the header height
+        $('#side-menu').height($(window).height()- $('#header').height());
+        if($('#side-menu').hasClass('active')) {
+            sideMenuToggle(false);
+            $(this).width($('.sidebar-left').width());
+        }
     });
     $(window).trigger('resize');
 
-    // slide-menu in/out
+    // side-menu active
     $('#toggle-side-menu').on('click', function(){
+        sideMenuToggle(true);
+    });
 
-        $('#toggle-side-menu').toggleClass('active');
-
-        if($('#side-menu').hasClass('side-menu-show')){
-
-            $('#side-menu').animate({
-                right: -300
-            }).removeClass('side-menu-show');
-
-            // main
-            $('#main').animate({
-                left: 0
-            });
-
-        }else{
-
-            // side-menu
-            $('#side-menu').animate({
-                right: 0
-            }).addClass('side-menu-show');
-
-            // main
-            $('#main').animate({
-                left: -300
-            });
-        }
+    // side-menu desactive
+    $('.side-menu-head-icon').on('click', function(){
+        sideMenuToggle(false);
+        // $('#side-menu').hide();
     });
 
     // side-menu li active class
@@ -41,8 +53,5 @@ $('document').ready(function(){
         $(this).toggleClass('active');
     });
 
-    // tooltip - add rel='tooltip' to the tooltip element
-    $(function () {
-        $("[rel='tooltip']").tooltip();
-    });
+
 });
