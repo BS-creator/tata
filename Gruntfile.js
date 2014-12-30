bd40760552a292c321381d827602090249f47f0d
Gilles Collard <gilles.collard@groups.be>
2014-12-30 12:04:33 +0100

    optimisation less cdn

--- Gruntfile.js
+++ Gruntfile.js
@@ -259,9 +259,8 @@ module.exports = function ( grunt ){
             production: {
                 options: {},
                 files  : {
-                  "public/css/portal-select2.css":    "public/less/portal-components/portal-select2.less",
-                  "public/css/portal-components.css": "public/less/portal-components/portal-components.less",
-                  "public/css/gst.css"              : "public/less/gst.less"
+                  "public/css/port-components.css": "public/less/port-components.less",
+                  "public/css/gst.css"            : "public/less/gst.less"
                 }
             }
         }
